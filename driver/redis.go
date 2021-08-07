d883c42f0218f6c309e7f3d255e4cb6667852be4
dezhizhang <1541609448@qq.com>
2021-08-07 18:14:40 +0800

    update

--- driver/redis.go
+++ driver/redis.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-redis/redis/v8"
 	"log"
 	"sync"
+	"time"
 )
 
 
@@ -14,16 +15,22 @@ var redisClientOnce sync.Once
 func Redis() *redis.Client {
 	redisClientOnce.Do(func() {
 		redisClient := redis.NewClient(&redis.Options{
-			Addr:     "127.0.0.1:6379",
-			Password: "", // no password set
-			DB:       0,  // use default DB
-			PoolSize:15,
+			Addr: "127.0.0.1:6379",
+			Password: "",
+			DB: 0,
+			PoolSize: 15,
+			MinIdleConns: 10,
+			DialTimeout: 5 * time.Second,
+			ReadTimeout: 3 * time.Second,
+			WriteTimeout: 3 * time.Second,
 		})
 		pong,err := redisClient.Ping(context.Background()).Result()
 		if err != nil {
-			log.Fatal(fmt.Errorf("connect error:%s",err))
+			log.Fatal(fmt.Errorf("连接失败:%err",err))
+			return
 		}
 		log.Fatal(pong)
 	})
 	return redisClient
+
 }
